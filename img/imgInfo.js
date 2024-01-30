16770644ccc24f75237a1822a0fb0e77345dcc9f
Arnette Reggie <mixedworldcitizen@gmail.com>
2024-01-30 19:15:36 +0100

    24 01 30, image sources

--- img/imgInfo.js
+++ img/imgInfo.js
@@ -7,7 +7,11 @@
  this is the source for all the pictures: 
 
  https://icomoon.io/app/#/select/library
- 
+
+In this project: FONT AWESOME library was used
+
+Choose just by pressing the needed item, 
+That is a huge collection
 
 
 
